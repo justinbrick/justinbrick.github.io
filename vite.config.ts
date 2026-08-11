dc595557a7178818f2b2b6da17413a664b3ec4ee
Justin <justin@justin.directory>
2026-08-11 17:25:56 -0400

    feat: add static generation

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+import { build } from '$service-worker';
+import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
@@ -14,12 +15,16 @@ export default defineConfig({
 					return !ignored.includes(warning.code);
 				}
 			},
-
-			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-			adapter: adapter()
+			adapter: adapter({
+				pages: 'build',
+				assets: 'build',
+				fallback: '404.html',
+				precompress: false,
+				strict: true
+			}),
+			paths: {
+				base: ''
+			}
 		})
-	],
-	base: '/'
+	]
 });
